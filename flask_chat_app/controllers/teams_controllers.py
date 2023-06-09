b2c09310beefe667d3b7a16a0e3ce2c4958e86d4
Marvis Knight <knightmar78@gmail.com>
2023-06-08 22:04:30 -0700

    first commit. Sandbox files

--- flask_chat_app/controllers/teams_controllers.py
+++ flask_chat_app/controllers/teams_controllers.py
@@ -7,6 +7,18 @@ from flask_chat_app.models.team_model import Team
 bcrypt = Bcrypt(app)
 # ------------------- Controllers/Routes for models ------------------->
 
+# API ROUTES
+@app.route('/api/teams/<int:id>/history')
+def get_team_history(id):
+    data = {
+        'id': id
+    }
+    return Team.get_history_by_id(data)
+
+@app.route('api/teams/<int:id>/leave')
+def remove_team(id):
+    pass
+
 @app.route('/dashboard')
 def dashboard():
     if 'user_id' not in session:
@@ -15,5 +27,6 @@ def dashboard():
         'id': session['user_id']
     }
     logged_user = User.get_by_id(user_data)
-    return render_template('dashboard.html', logged_user=logged_user)
+    public_teams = Team.get_public_team()
+    return render_template('dashboard.html', logged_user=logged_user, public_teams=public_teams)
 
